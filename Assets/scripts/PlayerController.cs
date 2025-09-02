6def3daa0d6925e0efb5e280e07c4021259766db
rune <oo8925442@gmail.com>
2025-09-02 14:11:15 +0900

    PlayerControllerの最初

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rbody; //PlayerについているRigidbody2Dを扱うための変数
+
+    float axisH; //入力の方向を記憶するための変数
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { 
+        rbody = GetComponent<Rigidbody2D>(); //Playerについているコンポーネント情報を取得
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            //Velocityの元となる値の取得(右なら1.0f、左なら-1.0f、何もなければ0)
+            axisH = Input.GetAxisRaw("Horizontal");
+
+        //velocytyに値を代入
+        rbody.linearVelocity = new Vector2(axisH,0);            
     }
 }
