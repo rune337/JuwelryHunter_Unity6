9aee0aedbc452c58ecb7593821554dc8b0b8683d
rune <oo8925442@gmail.com>
2025-09-03 13:26:15 +0900

    地面判定とジャンプの完成

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,13 +4,23 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody; //PlayerについているRigidbody2Dを扱うための変数
 
-    float axisH; //入力の方向を記憶するための変数
+    [Header("プレイヤーの能力値")]
     public float speed = 3.0f; //プレイヤーのスピードを調整
+    public float jumpPower  = 9.0f; //ジャンプ力
+
+    [Header("地面判定の対象レイヤー")]
+    public LayerMask groundLayer;
+
+    float axisH; //入力の方向を記憶するための変数
+    bool goJump = false; //ジャンプフラグ (treu　真on　false 偽off)
+    bool onGround = false; //地面にいるかどうかの判定 (いる:true、地面にいない:false)
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>(); //Playerについているコンポーネント情報を取得
+        
 
     }
 
@@ -20,10 +30,22 @@ public class PlayerController : MonoBehaviour
         //Velocityの元となる値の取得(右なら1.0f、左なら-1.0f、何もなければ0)
         axisH = Input.GetAxisRaw("Horizontal");
 
+        if (axisH > 0)
+        {
+            //右を向く
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if(axisH < 0)
+        {
+            //左を向く
+            transform.localScale = new Vector3(-1,1,1);
+        }
 
-
-
-
+        //GetButtonDownメソッド→引数に指定したボタンが押されたらtrueを返す、押されていなければfalse
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump(); //Jumpメソッドの発動
+        }
 
 
     }
@@ -31,8 +53,37 @@ public class PlayerController : MonoBehaviour
     //1秒間に50回繰り返すように制御しながら行う繰り返しメソッド
     private void FixedUpdate()
     {
+
+        //地面判定をサークルキャストで行って、その結果を変数onGroundに代入
+        onGround = Physics2D.CircleCast(
+            transform.position, //発射位置=プレイヤーの位置 (基準点)
+            0.2f,               //調査する円の半径
+            new Vector2(0,1.0f),//発射方向　※下方向
+            0,                  //発射距離 
+            groundLayer         //対象となるレイヤー情報 
+            );
+
+
         //velocytyに値を代入
         rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
+
+        //ジャンプフラグが立ったら
+        if(goJump)
+        {
+            //ジャンプさせる→プレイヤーを上に押し出す
+            rbody.AddForce(new Vector2(0,jumpPower),ForceMode2D.Impulse);
+            goJump  = false;
+        }
+
+
     }
 
+    //ジャンプフラグがおされたときに呼び出されるメソッド
+    void Jump()
+    {
+        if (onGround)
+        {
+            goJump = true; //true:ジャンプフラグをON
+        }
+    }
 }
